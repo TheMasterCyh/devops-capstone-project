112747480555ea1a2898f269303dd04ee7d25744
TheMasterCyh <TheMasterCyh@users.noreply.github.com>
2026-09-18 17:45:19 -0400

    Implement read account endpoint

--- service/routes.py
+++ service/routes.py
@@ -50,9 +50,11 @@ def create_accounts():
     account.deserialize(request.get_json())
     account.create()
     message = account.serialize()
-    # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    location_url = "/"  # Remove once get_accounts has been implemented
+    location_url = url_for(
+        "get_accounts",
+        account_id=account.id,
+        _external=True,
+    )
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
@@ -68,7 +70,20 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """Retrieve a single Account"""
+    app.logger.info("Request to Retrieve an Account with id [%s]", account_id)
+
+    account = Account.find(account_id)
+
+    if not account:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id '{account_id}' was not found.",
+        )
+
+    return jsonify(account.serialize()), status.HTTP_200_OK
 
 
 ######################################################################
